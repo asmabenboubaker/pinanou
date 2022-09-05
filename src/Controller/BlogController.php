6e5d892e70fe07ecd37597c8fe4f2d1e5d09a282
Asma <asmaboubaker11@gmail.com>
2022-09-05 03:50:15 +0200

    crud

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Blog;
+use App\Form\BlogType;
+use App\Repository\BlogRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/blog")
+ */
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_blog_index", methods={"GET"})
+     */
+    public function index(BlogRepository $blogRepository): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'blogs' => $blogRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="app_blog_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, BlogRepository $blogRepository): Response
+    {
+        $blog = new Blog();
+        $form = $this->createForm(BlogType::class, $blog);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $imageFile = $form->get('image')->getData();
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $newFilename = $originalFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+                try {
+                    $imageFile->move(
+                        'back\images',
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                }
+                $blog->setImage($newFilename);
+            }
+            $blogRepository->add($blog);
+            return $this->redirectToRoute('app_blog_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('blog/new.html.twig', [
+            'blog' => $blog,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_blog_show", methods={"GET"})
+     */
+    public function show(Blog $blog): Response
+    {
+        return $this->render('blog/show.html.twig', [
+            'blog' => $blog,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_blog_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Blog $blog, BlogRepository $blogRepository): Response
+    {
+        $form = $this->createForm(BlogType::class, $blog);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $blogRepository->add($blog);
+            return $this->redirectToRoute('app_blog_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('blog/edit.html.twig', [
+            'blog' => $blog,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_blog_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Blog $blog, BlogRepository $blogRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$blog->getId(), $request->request->get('_token'))) {
+            $blogRepository->remove($blog);
+        }
+
+        return $this->redirectToRoute('app_blog_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Entity/Blog.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BlogRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BlogRepository::class)
+ */
+class Blog
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $descri;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Categorie::class, inversedBy="blogs")
+     */
+    private $categorie;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescri(): ?string
+    {
+        return $this->descri;
+    }
+
+    public function setDescri(?string $descri): self
+    {
+        $this->descri = $descri;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?categorie
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(?categorie $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+}
